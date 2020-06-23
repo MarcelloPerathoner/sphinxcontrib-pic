4036ddd659e9fafa33945c725b376d1c1dc620eb
Marcello Perathoner <marcello@perathoner.de>
2020-06-23 15:25:00 +0200

    Add capability to handle plain text.

--- sphinxcontrib/pic/__init__.py
+++ sphinxcontrib/pic/__init__.py
@@ -3,14 +3,21 @@
 
     Draw pictures with 'little languages'.
 
-    Originally used to draw diagrams with GNU pic.  Hence the name.  Now works
-    with many 'little languages', eg.  graphviz dot, GNU pic, plantuml, ...
+    Originally I used this to draw diagrams with GNU pic.  Hence the name.  Now
+    it works with many 'little languages', eg.  graphviz dot, GNU pic, plantuml,
+    ...
+    It is not limited to pictures either, you can insert the output of any
+    commandline program into your documentation.
 
     To work with this directive the 'little language' program must:
 
-    - output SVG,
     - be callable from the commandline,
     - read from stdin and write to stdout.
+
+    If the program cannot read from stdin or write to stdout you can try these
+    pseudo-files: :file:`-` or :file:`/dev/stdin` for stdin and
+    :file:`/dev/stdout` for stdout.  For stubborn cases call the program from
+    inside a shell script that writes and reads temporary files.
 
     Add to your conf.py
 
@@ -25,25 +32,45 @@
 
           pic_options = {
               'dot' : {
-                  'program' : ["dot", "-Tsvg"],
-                  'align'   : "center",
-                  'prolog'  : "",
-                  'epilog'  : "",
+                  'program' : ['dot', '-Tsvg'],
+                  'align'   : 'center',
               },
               'pic' : {
-                  'program' : "m4 | dpic -v",
+                  'program' : 'm4 | dpic -v',
                   'shell'   : True,
-                  'align'   : "center",
-                  'prolog'  : ".PS\n",
-                  'epilog'  : "\n.PE\n",
+                  'align'   : 'center',
+                  'prolog'  : '.PS\n',
+                  'epilog'  : '\n.PE\n',
               },
               'uml' : {
-                  'program' : ["plantuml", "-tsvg", "-p"],
-                  'align'   : "center",
-                  'prolog'  : "@startuml\n",
-                  'epilog'  : "\n@enduml\n",
+                  'program' : ['plantuml', '-tsvg', '-p'],
+                  'align'   : 'center',
+                  'prolog'  : '@startuml\n',
+                  'epilog'  : '\n@enduml\n',
+              },
+              'tree' : {
+                  'program'     : ['xargs', 'tree', '-l', '--noreport', '-I', '*~', '-I', '__pycache__'],
+                  'format'      : 'text/plain',
+                  'html-prolog' : '<div class="highlight"><div class="highlight"><pre>',
+                  'html-epilog' : '</pre></div></div>',
               },
           }
+
+    Add a separate entry into the :code:`pic_options` dictionary
+    for every little language you plan to use.
+
+    :param string program: The commandline and arguments as list.
+                           The little language code goes into stdin.
+    :param string align: How to align the output.
+    :param string shell: Boolean. Use a shell (eg. for piping).
+    :param string format: The format of the output. ('text/xml', 'text/plain' or 'image/png')
+                          Defaults to 'text/xml'.
+    :param string caption: Output a figure with this caption.
+    :param string prolog: The little language code to prepend to all program input.
+    :param string epilog: The little language code to append to all program input.
+    :param string html-classes: Space separated list of classes to add to the top <div>.
+    :param string html-prolog: The HTML code to prepend to all program output.
+    :param string html-epilog: The HTML code to append to all program output.
 
     In your documentation use:
 
@@ -66,11 +93,13 @@
 
              client -> server
 
+          .. pic:: tree
+             :caption: Server directory structure
 
-    If the program cannot read from stdin or write to stdout you can try these
-    pseudo-files: :file:`-` or :file:`/dev/stdin` for stdin and
-    :file:`/dev/stdout` for stdout.  For stubborn cases call the program from
-    inside a shell script that writes and reads temporary files.
+             ../server
+
+    The argument of the directive specifies the little language to use.
+    The parameters are the same as above.
 
     If you need even more power you can pipe your code through a pre-processor
     like the M4 macro processor, or post-process the SVG output with a tool like
@@ -81,6 +110,7 @@
 
 """
 
+import base64
 import re
 import subprocess
 import os
@@ -96,12 +126,13 @@ from sphinx.util import logging
 from sphinx.util.docutils import nodes, SphinxDirective, SphinxTranslator
 from sphinx.util.nodes import set_source_info
 from sphinx.errors import SphinxWarning, SphinxError, ExtensionError
+from sphinx.util.logging import getLogger
 from sphinx.writers.html import HTMLTranslator
 from sphinx.writers.latex import LaTeXTranslator
 
 import pbr.version
 
-logger = logging.getLogger (__name__)
+logger = getLogger (__name__)
 
 if False:
     # For type annotations
@@ -120,11 +151,23 @@ class PicError (SphinxError):
 
 class PicNode (nodes.General, nodes.Inline, nodes.Element):
 
-    def exec_pic (self, proc, stdin) -> str:
-        """Low-level exec the program and handle errors"""
+    def render (self, code: str) -> bytes:
+        """Render PIC code.
+
+        Exec the program and handle errors.
+        """
+
+        proc = subprocess.Popen (
+            self['options']['program'],
+            stdin    = subprocess.PIPE,
+            stdout   = subprocess.PIPE,
+            stderr   = subprocess.PIPE,
+            cwd      = self['options']['cwd'],
+            shell    = self['options']['shell'],
+        )
 
         try:
-            stdout, stderr = proc.communicate (stdin, timeout = 15)
+            stdout, stderr = proc.communicate (code.encode ('utf-8'), timeout = 15)
 
         except subprocess.CalledProcessError as exc:
             raise PicError (__ ('The pic program exited with error:\n[stderr]\n%r\n'
@@ -137,68 +180,56 @@ class PicNode (nodes.General, nodes.Inline, nodes.Element):
         except OSError as exc:
             raise PicError (__ ('The pic program %r cannot be run '
                                 'check the pic_program setting') % program)
-        if stderr != '':
+        if stderr:
+            logger.info (__ ('The PIC Code was: %s') % code)
             raise PicError (__ ('The pic program produced errors:\n[stderr]\n%s\n') % stderr)
 
         return stdout
-
-
-    def render_xml (self) -> str:
-        """Render PIC code into HTML or standalone SVG"""
-
-        proc = subprocess.Popen (
-            self['options']['program'],
-            stdin    = subprocess.PIPE,
-            stdout   = subprocess.PIPE,
-            stderr   = subprocess.PIPE,
-            encoding = 'utf-8',
-            cwd      = self['options']['cwd'],
-            shell    = self['options']['shell'],
-        )
-        stdout = self.exec_pic (proc, self['code'])
-
-        # fix standalone SVG for embedding
-        stdout = re.sub (r'<\?xml .*?\?>',  '', stdout, flags = re.S)
-        stdout = re.sub (r'<!DOCTYPE .*?>', '', stdout, flags = re.S)
-
-        # stupid plantuml sets dimensions in a style on the element
-        # so you cannot override them in CSS
-        stdout = re.sub (r'(<svg .*?)style=".*?"', r'\1', stdout, flags = re.S)
-
-        return stdout
-
-
-    def render_bin (self) -> str:
-        """Render PIC code into something binary like PNG"""
-
-        proc = subprocess.Popen (
-            self['options']['program'],
-            stdin    = subprocess.PIPE,
-            stdout   = subprocess.PIPE,
-            stderr   = subprocess.PIPE,
-            cwd      = self['options']['cwd'],
-            shell    = self['options']['shell'],
-        )
-        return self.exec_pic (proc, self['code'].encode ('utf-8'))
 
 
     def html_visit (self, translator: HTMLTranslator):
 
         classes = ['pic']
         align   = ''
+        format_ = self['options']['format']
 
         if 'align' in self:
             classes.append ('align-%s' % self['align'])
             align = 'align="%s"' % self['align']
 
-        if 'format' in self['options']:
-            classes.append ('pic-format-%s' % self['options']['format'])
+        classes.append ('pic-format-%s'   % format_.replace ('/', '-'))
+        classes.append ('pic-language-%s' % self['options']['language'])
 
         if 'html-classes' in self['options']:
             classes += self['options']['html-classes'].split ()
 
         translator.body.append ('<div %s class="%s">\n' % (align, ' '.join (classes)))
-        translator.body.append (self.render_xml ())
+        translator.body.append (self['options']['html-prolog'])
+
+        if format_ == 'text/xml':
+            stdout = self.render (self['code']).decode ('utf-8')
+
+            # fix standalone SVG for embedding
+            stdout = re.sub (r'<\?xml .*?\?>',  '', stdout, flags = re.S)
+            stdout = re.sub (r'<!DOCTYPE .*?>', '', stdout, flags = re.S)
+
+            # stupid plantuml sets dimensions in a style on the element
+            # so you cannot override them in CSS
+            stdout = re.sub (r'(<svg .*?)style=".*?"', r'\1', stdout, flags = re.S)
+
+            translator.body.append (stdout)
+
+        if format_ == 'text/plain':
+            stdout = self.render (self['code']).decode ('utf-8')
+            translator.body.append (stdout)
+
+        if format_ == 'image/png':
+            stdout = self.render (self['code'])
+            translator.body.append (
+                '<img src="data:image/png;base64,%s" />\n' % base64.b64encode (stdout)
+            )
+
+        translator.body.append (self['options']['html-epilog'])
         translator.body.append ('</div>\n')
 
         raise nodes.SkipNode
@@ -222,13 +253,17 @@ class PicDirective (SphinxDirective):
 
     base_option_spec = {
         'align'        : lambda v: directives.choice (v, ('left', 'center', 'right')),
-        'format'       : lambda v: directives.choice (v, ('dot', 'html')),
+        'format'       : lambda v: directives.choice (v, ('text/xml', 'text/plain', 'image/png')),
         'caption'      : directives.unchanged,
         'name'         : directives.unchanged,
         'html-classes' : directives.unchanged,
+        # the prolog and epilog to add around the program output
+        'html-prolog'  : directives.unchanged,  # prolog (overrides conf.py)
+        'html-epilog'  : directives.unchanged,  # epilog (overrides conf.py)
     }
 
     option_spec = {
+        # the prolog and epilog to add around the little language code
         'prolog'       : directives.unchanged,  # prolog (overrides conf.py)
         'epilog'       : directives.unchanged,  # epilog (overrides conf.py)
     }
@@ -257,7 +292,7 @@ class PicDirective (SphinxDirective):
         # opt = self.options.get (name) or getattr (self.env.config, "%s_%s" % (self.name, name))
 
         options  = getattr (self.env.config, self.name + '_options')
-        language = self.arguments[0]
+        language = self.options['language']
         if language not in options:
             raise PicError (__ ('Unknown language %s in directive.') % language)
 
@@ -272,6 +307,7 @@ class PicDirective (SphinxDirective):
 
     def get_code (self):
         """ Get the PIC code. """
+
         if len (self.arguments) > 1:
             if self.content:
                 return self.err (__ ('The PIC directive cannot have both content and a filename argument.')),
@@ -287,11 +323,14 @@ class PicDirective (SphinxDirective):
             pic_code = '\n'.join (self.content)
             if not pic_code.strip ():
                 return self.err (__ ('Found PIC directive without content.'))
-        return self.get_opt ('prolog') + pic_code + self.get_opt ('epilog')
+
+        prolog = self.get_opt ('prolog') or ''
+        epilog = self.get_opt ('epilog') or ''
+        return prolog + pic_code + epilog
 
 
-    def run (self) -> List[PicNode]:
-        """ Turn the directive into a node. """
+    def _run (self):
+        """ Turn the directive into nodes. """
 
         node = PicNode ()
         node['code']    = self.get_code ().strip ()
@@ -300,8 +339,11 @@ class PicDirective (SphinxDirective):
             'shell'        : self.get_opt ('shell') or False,
             'cwd'          : self.get_opt ('cwd'),
             'alt'          : self.get_opt ('alt'),
-            'format'       : self.get_opt ('format') or 'dot',
+            'language'     : self.get_opt ('language'),
+            'format'       : self.get_opt ('format') or 'text/xml',
             'html-classes' : self.get_opt ('html-classes') or '',
+            'html-prolog'  : self.get_opt ('html-prolog') or '',
+            'html-epilog'  : self.get_opt ('html-epilog') or '',
         }
 
         node['align'] = self.get_opt ('align')
@@ -316,6 +358,11 @@ class PicDirective (SphinxDirective):
             figure = self.figure_wrapper (node, self.options['caption'])
             self.add_name (figure)
             return [figure]
+
+
+    def run (self):
+        self.options['language'] = self.arguments[0]
+        return self._run ()
 
 
 def html_visit_pic (translator: HTMLTranslator, node: PicNode) -> None:
